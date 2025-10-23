d1cfdcefba875e44e7b252adbe7d65d8bcd8d0a4
yww <1141950370@qq.com>
2025-10-23 08:24:36 +0000

    🎨 修改了rss生成方式和模板，并进行下优化，修复移动端侧边栏显示问题

--- yww/optimize.js
+++ yww/optimize.js
@@ -24,6 +24,6 @@ document.addEventListener('DOMContentLoaded', () => {
     /**
      * 打印欢迎语
      */
-    console.info("%c yww %c 欢迎来到我的博客！", "padding: 2px 6px; border-radius: 3px 0 0 3px; color: #fff; background: #8EC5FC; font-weight: bold;", "padding: 2px 6px; border-radius: 0 3px 3px 0; color: #fff; background: #CD9DFA; font-weight: bold;");
+    console.info("%c whyne %c 欢迎来到我的博客！", "padding: 2px 6px; border-radius: 3px 0 0 3px; color: #fff; background: #8EC5FC; font-weight: bold;", "padding: 2px 6px; border-radius: 0 3px 3px 0; color: #fff; background: #CD9DFA; font-weight: bold;");
 
 });
